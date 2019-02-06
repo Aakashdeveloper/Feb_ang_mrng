c994abcd7388ebddd4493bc5ebf1f869988633ae
aakashDeveloper <ahanda205@gmail.com>
2019-02-06 07:08:01 +0530

    a

--- firstapp/src/app/products/product.component.ts
+++ firstapp/src/app/products/product.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class ProductComponent {
     title: String = '~~Product List~~';
     filterText: String  = 'Number Of Product Filter:';
+    showImage: Boolean = false;
     products: any[] = [
 
         {    '_id': '5a05dacc734d1d68d42d31f3',
@@ -32,15 +33,20 @@ export class ProductComponent {
             'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
         },
         {
-            '_id': '5a05daec734d1d68d42d32ca',
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+            '_id': '5a05dacc734d1d68d42d31f3',
+            'productId': 1,
+            'productName': 'Leaf Rake',
+            'productCode': 'GDN-0011',
+            'releaseDate': 'March 19, 2016',
+            'description': 'Leaf rake with 48-inch wooden handle.',
+            'price': 19.95,
+            'starRating': 3.5,
+            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
         }
     ];
+
+    // show hide image
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
