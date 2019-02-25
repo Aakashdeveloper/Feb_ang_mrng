c9944da271d6fc816b5e90e04177d335d052ce5f
aakashDeveloper <ahanda205@gmail.com>
2019-02-25 07:56:55 +0530

    a

--- firstapp/src/app/products/product-detail.component.ts
+++ firstapp/src/app/products/product-detail.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
     templateUrl: './product-detail.component.html'
 })
 
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+    id: Number;
+    name: String;
+    desc: String;
+    imageUrl: String;
+
+    constructor(private _route: ActivatedRoute,
+            private _router: Router) {}
+
+    ngOnInit(): void {
+        this.id = this._route.snapshot.params['id'];
+        this._route.queryParams
+            .subscribe((data) => {
+                this.name = data['name'];
+                this.desc = data['description'];
+                this.imageUrl = data['img'];
+            });
+    }
+
+    onBack(): void {
+        this._router.navigate(['/products']);
+    }
 
 }
