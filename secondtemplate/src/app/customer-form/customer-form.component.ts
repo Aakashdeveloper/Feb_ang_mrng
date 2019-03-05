62fed928e2533b681d2ab3234a0aefb2404d3deb
aakashDeveloper <ahanda205@gmail.com>
2019-03-05 06:52:11 +0530

    a

--- secondtemplate/src/app/customer-form/customer-form.component.ts
+++ secondtemplate/src/app/customer-form/customer-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-customer-form',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class CustomerFormComponent implements OnInit {
 
   languages: any[] = ['AngularJs', 'NodeJs', 'Python'];
+  model = new Employee('John', 'Andy', true, 'male', 'NodeJs');
+
+  firstToUpper(value: string) {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ secondtemplate/src/app/models/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public fulltime: boolean,
+        public gender: string,
+        public codelang: string
+    ) {
+
+    }
+}
