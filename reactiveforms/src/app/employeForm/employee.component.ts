4a68cbed5626eac9b2700f0fb18f6a0f1680c999
aakashDeveloper <ahanda205@gmail.com>
2019-03-13 06:50:24 +0530

    a

--- reactiveforms/src/app/employeForm/employee.component.ts
+++ reactiveforms/src/app/employeForm/employee.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder,
+         Validators,
+         AbstractControl } from '@angular/forms';
+
+function ratingRange(min: Number, max: Number) {
+    return(c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+                return {'range': true};
+            }
+                return null;
+            };
+}
 
 @Component({
     selector: 'app-reactive',
     templateUrl: './employee.component.html'
 })
+
+
 
 export class EmployeeFormComponent implements OnInit {
     employeeForm: FormGroup;
@@ -17,7 +30,10 @@ export class EmployeeFormComponent implements OnInit {
         this.employeeForm = this.fb.group({
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['', [Validators.required, Validators.maxLength(5)]],
-            email: ['', [Validators.required, Validators.pattern('[0-9a-zA-Z]+@[a-zA-Z0-9]+')]]
+            email: ['', [Validators.required, Validators.pattern('[0-9a-zA-Z]+@[a-zA-Z0-9]+')]],
+            rating: ['', [Validators.required, ratingRange(1, 5)]],
+            phone: [''],
+            notification: []
         });
     }
 
@@ -25,6 +41,16 @@ export class EmployeeFormComponent implements OnInit {
         this.employeeForm.patchValue({
             email: 'j@a.com'
         });
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.employeeForm.get('phone');
+        if (notifyVia === 'phone') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
 
     save(): void {
